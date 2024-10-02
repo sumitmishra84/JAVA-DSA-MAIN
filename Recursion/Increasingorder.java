bd66cfd3b152b87ff8520f8a4441e9b88771289d
Sumit Mishra <sm0141136@gmail.com>
2024-10-02 20:08:32 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Recursion/Increasingorder.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Increasingorder {
+    public static void printInc(int n){
+        if (n==1){
+            System.out.println(n);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+" ");
+    }
+    public static void main(String[] args) {
+        int n=10;
+        printInc(n);
+    }
+}
